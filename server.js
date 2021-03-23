6b1a86b58e741e7ab0dd4bd035deadb1b92632b5
Konstantin <const.trofimov@gmail.com>
2021-03-23 19:17:24 +0300

    make it better

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express')
 const serverStatic = require('serve-static')
 const path = require('path')
 
+const app = express()
+
 app.use('/', serverStatic(path.join(__dirname, '/dist')))
 
 const port = process.env.PORT || 8080
